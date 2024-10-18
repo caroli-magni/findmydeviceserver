270db924e6f99405508723b88191d160d0b08b06
carolimagni <45108061+caroli-magni@users.noreply.github.com>
2024-10-18 15:43:29 +0100

    Update access.go changed index variable to _ to be unused

--- user/access.go
+++ user/access.go
@@ -78,7 +78,7 @@ func (a *AccessController) ResetLock(id string) {
 }
 
 func (a *AccessController) CheckAccessToken(tokenToCheck string) (string, error) {
-	for id := range a.accessTokens {
+	for _, id := range a.accessTokens {
 		if id.Token == tokenToCheck {
 			//tokenExpired := id.ExpirationTime < time.Now().Unix()
 			// if tokenExpired {
